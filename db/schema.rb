92d2015793506ace971778a79cc970b8903686e3
bingolcl <cc23zhang@gmail.com>
2016-01-11 18:43:43 -0700

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160110221816) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "colour"
+    t.string   "color"
     t.integer  "price"
   end
 
