46abf96bcada817688435e0983a9b22240eb7bbd
bingolcl <cc23zhang@gmail.com>
2016-01-14 20:55:42 -0700

    Order update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,48 @@
 class OrdersController < ApplicationController
+  before_action :set_product, only: [:show, :new, :create, :destroy]
 
   def index
+    @orders = Order.all
   end
 
   def show
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+        @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+        @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
-end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:user_id, :product_id, :total,)
+    end
+  end
+
