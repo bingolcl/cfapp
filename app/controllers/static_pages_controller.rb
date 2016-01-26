d1ff30021a50a7c439df9d163ff37e6aa85a75f3
bingolcl <cc23zhang@gmail.com>
2016-01-25 19:11:22 -0700

    contact/email feature update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+
+  def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	UserMailer.contact_form(@email, @name, @message).deliver
+  end
 	# layout "static"
   def landing_page
   @products = Product.limit(3)
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,14 +6,6 @@ class UserMailer < ActionMailer::Base
   	mail(:from => email, 
 	  	 :to => 'cc23zhang@gmail.com', 
 	  	 :subject => "A new contact form message from #{name}")
-  end
-
-
-   def thank_you
-     @name = params[:name]
-     @email = params[:email]
-     @message = params[:message]
-     UserMailer.contact_form(@email, @name, @message).deliver
-  end
+   end
 
 end
