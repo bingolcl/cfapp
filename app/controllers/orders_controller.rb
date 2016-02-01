7ced896855201e21b62525a410871650f6463389
bingolcl <cc23zhang@gmail.com>
2016-01-31 20:21:08 -0700

    review branch added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,17 @@
 class OrdersController < ApplicationController
   before_action :set_product, only: [:show, :new, :create, :destroy]
   before_filter :authenticate_user!
+  protect_from_forgery with: :null_session
+  respond_to :json, :html
 
   def index
-    @orders = Order.all
+    @orders = Order.all.to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
+    respond_with @orders
   end
 
   def show
+    @order = Order.find(params[:id]).to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
+    respond_with @order
   end
 
   def new
@@ -24,14 +29,10 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @order = Order.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:product_id, :product_id, :total,)
+      params.require(:order).permit(:product_id, :user_id, :total)
     end
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,11 +11,13 @@ class ProductsController < ApplicationController
     else
     @products = Product.all
     end
+    respond_with @products
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    # @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
 
   # GET /products/new
