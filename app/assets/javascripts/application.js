25f9fe31c8ab10d1c2ef5bf0931f1b82b434fa3a
bingolcl <cc23zhang@gmail.com>
2016-03-20 16:20:18 -0600

    elevateZoom added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require jquery.raty
 //= require site
+//= require jquery.elevateZoom-3.0.8.min
 
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -5,5 +5,22 @@ $(document).on('ready page:load', function(){
       score: function() {
         return $(this).attr('data-score');
       }
-    });
+    }); // Raty rating stars
+
+    	$('.img-zoom').elevateZoom({
+			
+			scrollZoom 		: true,
+			borderSize		: 3,
+			borderColour 		: "rgb(136,136,136)",
+			tintColour 		: "#e7e7e7",
+			zoomWindowWidth 	: 500,
+			zoomWindowHeight 	: 300,
+			zoomWindowPosition	: 1,
+			zoomWindowOffetx	: 180,
+			zoomWindowOffety	: 0,
+			zoomWindowFadeIn	: 500,
+			zoomWindowFadeOut	: 500,
+			lensFadeIn		: 500,
+		lensFadeOut		: 500 
+	}); // elevateZoom on show page
 });
